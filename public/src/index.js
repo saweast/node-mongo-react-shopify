94183ab6c3073d8aa5663a7e55ffe69461ae8ad3
Vadym Lavorchuk <lavorchukvadim@gmail.com>
2020-03-23 11:40:13 +0200

    Simple routing and polaris

--- public/src/index.js
+++ public/src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import '@shopify/polaris/styles.css';
+import { AppProvider } from '@shopify/polaris';
+import enTranslations from '@shopify/polaris/locales/en.json';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import {
+  BrowserRouter as Router,
+} from "react-router-dom";
+
+function WrappedApp() {
+  return (
+    <Router>
+      <AppProvider i18n={enTranslations}>
+        <App />
+      </AppProvider>
+    </Router>
+  );
+}
+
+ReactDOM.render(<WrappedApp  />, document.getElementById('root'));
